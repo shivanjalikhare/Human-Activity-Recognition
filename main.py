b9f5ec75ce8c6d39dab47a814f176251f41f6681
Shivanjali Khare <shivanjali.khare@gmail.com>
2019-09-26 16:49:00 -0500

    2 features extracted
    mean and variance from 4 signals (x,y,z and total acc)

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
 
+
 Column_Names = ['user', 'activity','timestamp','x-axis','y-axis','z-axis']
 Labels = ['Downstairs','Jogging','Sitting','Standing','Upstairs','Walking']
 Data_path = 'data/WISDM_ar_v1.1_raw.txt'
@@ -27,14 +28,14 @@ data=data.dropna()
 #print(data.head())
 
 #show graph for any activity
-#data[data['activity'] == 'Standing'][['x-axis']][:50].plot(subplots=True,figsize=(10,8),title='Standing')
-#data[data['activity'] == 'Jogging'][['x-axis']][:50].plot(subplots=True,figsize=(10,8),title='Jogging')
-#plt.xlabel('Timestep')
-#plt.ylabel('x acc (dg)')
+data[data['activity'] == 'Standing'][['x-axis']][:50].plot(subplots=True,figsize=(10,8),title='Standing')
+data[data['activity'] == 'Jogging'][['x-axis']][:50].plot(subplots=True,figsize=(10,8),title='Jogging')
+plt.xlabel('Timestep')
+plt.ylabel('x acc (dg)')
 
 #activity graph
-#activity_type = data['activity'].value_counts().plot(kind='bar', title='frequency of activity')
-#plt.show()
+activity_type = data['activity'].value_counts().plot(kind='bar', title='frequency of activity')
+plt.show()
 
 #data_after_window_segmentation 
 data_after_window_segmentation = []
@@ -64,7 +65,45 @@ for i in range(0, len(data_after_window_segmentation)):
     a = np.sqrt(np.sum(np.square(data_after_window_segmentation[:i,:i])))
         
     total_acc.append(a)
+    
+#print(len(total_acc))
+#print(total_acc)
         
+x = data_after_window_segmentation[:,:,0]
+y = data_after_window_segmentation[:,:,1]
+z = data_after_window_segmentation[:,:,2]
 
-print(len(total_acc))
-print(total_acc)
+x_mean = np.mean(x)
+y_mean = np.mean(y)
+z_mean = np.mean(z)
+total_acc_mean = np.mean(total_acc)
+print('x_mean',x_mean)
+#print('---------------------------------------------')
+print('y_mean',y_mean)
+#print('---------------------------------------------')
+print('z_mean',z_mean)
+#print('---------------------------------------------')
+print('total_acc_mean',total_acc_mean)
+
+x_var = np.var(x, dtype=np.float32)
+y_var = np.var(y, dtype=np.float32)
+z_var = np.var(z, dtype=np.float32)
+total_acc_var = np.var(total_acc, dtype=np.float32)
+print('x_var',x_var)
+#print('---------------------------------------------')
+print('y_var',y_var)
+#print('---------------------------------------------')
+print('z_var',z_var)
+#print('---------------------------------------------')
+print('total_acc_var',total_acc_var)
+
+
+
+#print(data_after_window_segmentation[0,0])
+#print(data_after_window_segmentation[:,:,0])
+    
+
+    
+
+
+    
