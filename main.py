a338f809aec22aa20ba350b09f2b1334dbed666b
Shivanjali Khare <shivanjali.khare@gmail.com>
2019-09-26 12:42:41 -0500

    sliding window and labels

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Created on Thu Sep 26 12:03:30 2019
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
 
 Column_Names = ['user', 'activity','timestamp','x-axis','y-axis','z-axis']
 Labels = ['Downstairs','Jogging','Sitting','Standing','Upstairs','Walking']
@@ -33,3 +35,22 @@ plt.ylabel('x acc (dg)')
 #activity graph
 activity_type = data['activity'].value_counts().plot(kind='bar', title='frequency of activity')
 plt.show()
+
+#data_after_window_segmentation 
+data_after_window_segmentation = []
+labels = []
+
+for i in range(0,len(data) - time_size_segment,time_step):
+    x = data['x-axis'].values[i:i + time_size_segment]
+    y = data['y-axis'].values[i:i + time_size_segment]
+    z = data['z-axis'].values[i:i + time_size_segment]
+    data_after_window_segmentation.append([x,y,z])
+    
+    label = stats.mode(data['activity'][i: i+time_size_segment])[0][0]
+    labels.append(label)
+    
+#convert the new data to numpy
+data_after_window_segmentation = np.asarray(data_after_window_segmentation, dtype=np.float32).transpose(0,2,1)   
+labels = np.asarray(pd.get_dummies(labels), dtype=np.float32)
+print("data_after_window_segmentation shape:",data_after_window_segmentation.shape)
+print("labels shape",labels.shape) 
